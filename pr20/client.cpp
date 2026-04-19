78b65b1504b332bab3f9d9e51e3cf9393d249b04
nightmare <eugen.the.fast@gmail.com>
2026-04-19 16:41:38 +0300

    client.cpp

--- pr20/client.cpp
+++ pr20/client.cpp
@@ -1,37 +1,52 @@
-#include <cstring>
 #include <iostream>
+#include <cstring>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 
 using namespace std;
 
-int main() {
+int main()
+{
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-
-    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
-        cout << "Ошибка подключения!" << endl;
+    if (clientSocket < 0)
+    {
+        cerr << "Ошибка создания сокета" << endl;
         return 1;
     }
 
-    int numbers[2];
+    sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &serverAddress.sin_addr);
+
+    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    {
+        cerr << "Ошибка подключения к серверу" << endl;
+        close(clientSocket);
+        return 1;
+    }
+    cout << "Подключение к серверу установлено." << endl;
+
+    double a, b;
     cout << "Введите первое число: ";
-    cin >> numbers[0];
+    cin >> a;
     cout << "Введите второе число: ";
-    cin >> numbers[1];
+    cin >> b;
 
-    send(clientSocket, numbers, sizeof(numbers), 0);
+    send(clientSocket, &a, sizeof(double), 0);
+    send(clientSocket, &b, sizeof(double), 0);
 
-    int sum;
-    recv(clientSocket, &sum, sizeof(sum), 0);
+    cout << "Числа отправлены серверу: " << a << " и " << b << endl;
 
-    cout << "Ответ от сервера (сумма): " << sum << endl;
+    double result;
+    recv(clientSocket, &result, sizeof(double), 0);
+
+    cout << "Результат (сумма) от сервера: " << result << endl;
 
     close(clientSocket);
+
     return 0;
 }
